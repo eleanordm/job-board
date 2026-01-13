63569d629925913ce78afb78c7337579ae73143d
Eleanor Davis Mullen <eleanordm83.com>
2026-01-13 13:45:57 +0000

    first commit

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,4 @@
+<article 
+    {{ $attributes->class(['border border-slate-300 bg-white p-4 shadow-md rounded']) }}>
+    {{ $slot }} 
+</article>
---
+++ resources/views/job/index.blade.php
@@ -0,0 +1,33 @@
+<x-layout>
+    @foreach ($jobs as $job)
+        <x-card class="mb-4">
+            <div class="flex justify-between mb-4">
+                <h2 class="text-lg font-medium">{{ $job->title }}</h2>
+                <div class="text-slate-500">
+                    £{{ number_format($job->salary) }}
+                </div>
+            </div>
+
+            <div class="mb-4 flex items-center justify-between text-sm text-slate-500">
+                <div class="flex space-x-4">
+                    <div>Company name</div>
+                    <div>{{ $job->location }}</div>
+                </div>
+                <div class="flex space-x-1 text-xs">
+                    <x-tag>{{ Str::ucfirst($job->experience) }}</x-tag>
+                    <x-tag>{{ $job->category }}</x-tag>
+                </div>
+            </div>
+            
+            <p class="text-sm text-slate-500 mb-4">
+                {!! nl2br(e($job->description)) !!}
+            </p>
+
+            <div>
+                <x-link-button :href="route('jobs.show', $job)">
+                    Show
+                </x-link-button>
+            </div>
+        </x-card>
+    @endforeach
+</x-layout>
