9066cd978187a157b2b30445d7b881ba0a0f990c
Eleanor Davis Mullen <eleanordm83.com>
2026-01-19 10:32:35 +0000

    Breadcrumbs

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,33 +1,14 @@
 <x-layout>
+    <x-breadcrumbs class="mb-4" 
+        :links="['Jobs' => route('jobs.index')]" />
+        
     @foreach ($jobs as $job)
-        <x-card class="mb-4">
-            <div class="flex justify-between mb-4">
-                <h2 class="text-lg font-medium">{{ $job->title }}</h2>
-                <div class="text-slate-500">
-                    £{{ number_format($job->salary) }}
-                </div>
-            </div>
-
-            <div class="mb-4 flex items-center justify-between text-sm text-slate-500">
-                <div class="flex space-x-4">
-                    <div>Company name</div>
-                    <div>{{ $job->location }}</div>
-                </div>
-                <div class="flex space-x-1 text-xs">
-                    <x-tag>{{ Str::ucfirst($job->experience) }}</x-tag>
-                    <x-tag>{{ $job->category }}</x-tag>
-                </div>
-            </div>
-            
-            <p class="text-sm text-slate-500 mb-4">
-                {!! nl2br(e($job->description)) !!}
-            </p>
-
+        <x-job-card class="mb-4" :$job>
             <div>
                 <x-link-button :href="route('jobs.show', $job)">
                     Show
                 </x-link-button>
             </div>
-        </x-card>
+        </x-job-card>
     @endforeach
 </x-layout>
