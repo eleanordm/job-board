a039493d4fbd060f843ec8b0e4040629bb4e7b44
Eleanor Davis Mullen <eleanordm83.com>
2026-02-18 14:30:25 +0000

    Employer

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -9,8 +9,9 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Employer extends Model
 {
-    /** @use HasFactory<\Database\Factories\EmployerFactory> */
     use HasFactory;
+
+    protected $fillable = ['company_name'];
 
     public function jobs(): HasMany 
     {
