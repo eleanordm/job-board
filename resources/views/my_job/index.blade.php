15a103d85cfd022f93a77adcff64c26ff53da22d
Eleanor Davis Mullen <eleanordm83.com>
2026-02-20 14:01:39 +0000

    Soft deletes

--- resources/views/my_job/index.blade.php
+++ resources/views/my_job/index.blade.php
@@ -25,11 +25,17 @@
                         </div>
                     </div>
                 @empty
-                    <div>No applications yet</div>
+                    <div class="mb-2">No applications yet</div>
                 @endforelse
 
-                <div class="flex space-x-2">
+                <div class="flex space-x-2 items-center">
                     <x-link-button href="{{ route('my-jobs.edit', $job) }}">Edit</x-link-button>
+
+                    <form action="{{ route('my-jobs.destroy', $job) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <x-button>Delete</x-button>
+                    </form>
                 </div>
             </div>
         </x-job-card>
